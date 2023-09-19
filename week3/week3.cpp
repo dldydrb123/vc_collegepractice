a60045827f5e0879db54e8b149182c756407c729
dldydrb123 <63383518+dldydrb123@users.noreply.github.com>
2023-09-19 15:56:25 +0900

    first

---
+++ week3/week3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+	string s2;
+
+	while (1)
+	{
+		int key;
+		
+		key = _getch();
+
+		s2 += to_string(key);
+		cout << s2 << "\n";
+		
+	}
+
+	return 0;
+}
