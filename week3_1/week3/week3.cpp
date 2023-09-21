8f423b8f032ab3b693ea0a6174fb5e8e673da96e
dldydrb123 <63383518+dldydrb123@users.noreply.github.com>
2023-09-21 15:07:21 +0900

    Update week3.cpp
    3rd commit

--- week3_1/week3/week3.cpp
+++ week3_1/week3/week3.cpp
@@ -1,19 +1,24 @@
-﻿#include <iostream>
+#include <iostream>
 #include <conio.h>
 #include <string>
 #include <chrono>
-
+//s
 using namespace std;
 
 int main(void)
 {
-    int input;
+    int input, code;
     string s2;
     chrono::steady_clock::time_point start_time;
     chrono::steady_clock::time_point current_time;
-
+    start_time = chrono::steady_clock::now();
     while (1)
     {
+        
+        // 현재 시간 기록
+        if (s2.empty()==0) {
+            current_time = chrono::steady_clock::now();
+        }
 
         input = _getch();
         if (input == 224)
@@ -21,33 +26,33 @@ int main(void)
             input = _getch();
         }
         s2 += to_string(input);
-
+        cout << s2 << endl;
         if (input == 27)
         {
             break;
         }
-        // 현재 시간 기록
-        current_time = chrono::steady_clock::now();
-
         if (s2 == "807797")
         {
             cout << "아도겐! =o" << "\n";
             s2.clear();
+            start_time = current_time;  // 시작 시간 갱신
         }
 
         if (s2 == "777777")
         {
             cout << "전진! =o" << "\n";
             s2.clear();
+            start_time = current_time;  // 시작 시간 갱신
         }
 
         if (s2 == "777775")
         {
             cout << "발사 =o" << "\n";
             s2.clear();
+            start_time = current_time;  // 시작 시간 갱신
         }
         // 0.7초(700ms) 이상 경과하면 초기화
-        if (chrono::duration_cast<chrono::milliseconds>(current_time - start_time).count() >= 700)
+        if (chrono::duration_cast<chrono::milliseconds>(current_time - start_time).count() >= 2000)
         {
             s2.clear();
 
