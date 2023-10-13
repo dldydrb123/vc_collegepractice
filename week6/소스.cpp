41f48d38d2e047dad4a518a502a36e95e10d87d8
dldydrb123 <dldydrb-40@naver.com>
2023-10-13 20:30:57 +0900

    first commit

--- week6/소스.cpp
+++ week6/소스.cpp
@@ -5,182 +5,95 @@
 #endif
 
 #include <windows.h>
-POINT point = { 50, 50 }; // 초기 점 위치
-POINT startPoint = { 0 }; // 진짜 씨발 내가 논리적으로 생각해낸걸 chatgpt가 거의 유사하게 짰는데 문법적으로 이상한 부분을 내가 고치지 못해서 10시부터 아침 6시까지 이지@랄을 했다는게 믿기지가 않다
-POINT endPoint = { 0 };
-POINT startPoint1 = { 0 };
-POINT endPoint1 = { 0 };
-int isMouseLButtonPressed = 0;
-int RightButtonActived = 0;
-RECT dragRect;
 
+POINT startPoint = { 0 };
+POINT endPoint = { 0 };
+int isKeyPressed = 0;
+
+RECT rect_user = { 5, 5, 10, 10 }; // 왼쪽 상단 좌표 (50, 50)에서 오른쪽 하단 좌표 (150, 150)까지의 사각형
+RECT rect_target = { 50, 50, 150, 150 }; // 왼쪽 상단 좌표 (50, 50)에서 오른쪽 하단 좌표 (150, 150)까지의 사각형
+
+
+bool RectIntersect(const RECT* rect1, const RECT* rect2)
+{
+    if (rect1->left < rect2->right && rect1->right > rect2->left &&
+        rect1->top < rect2->bottom && rect1->bottom > rect2->top)
+    {
+        return true; // 두 RECT가 겹침
+    }
+    return false; // 두 RECT가 겹치지 않음
+}
 // 윈도우의 이벤트를 처리하는 콜백(Callback) 함수.
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-    RECT rc2;
-    POINT it, rb;
-    GetClientRect(hwnd, &rc2);
-    it.x = rc2.left; it.y = rc2.top-100; rb.x = rc2.right; rb.y = rc2.bottom;
-    ClientToScreen(hwnd, &it); 
-    ClientToScreen(hwnd, &rb);
-    rc2.left = it.x; rc2.top = it.y; rc2.right = rb.x; rc2.bottom = rb.y;
-    ClipCursor(&rc2);
+    HDC hdc = GetDC(hwnd);
+
+    HBRUSH Brush_user = CreateSolidBrush(RGB(0, 0, 255));
+    HBRUSH Brush_target = CreateSolidBrush(RGB(255, 0, 255));
+    const wchar_t* text = L"Crash!!!";
 
     switch (uMsg)
     {
-    case WM_LBUTTONDOWN:
-    {
-        startPoint.x = LOWORD(lParam);
-        startPoint.y = HIWORD(lParam);
-        isMouseLButtonPressed = 1;
-    }
-    break;
-
-    case WM_RBUTTONDOWN:
-    {
-        // 마우스 오른쪽 버튼 클릭 이벤트 처리
-        POINTS pt = MAKEPOINTS(lParam);
-        POINT clientPoint = { pt.x, pt.y };
-        ScreenToClient(hwnd, &clientPoint);
-
-        // 클라이언트 좌표를 기반으로 어떤 객체를 클릭했는지 확인하는 코드를 추가합니다.
-        // 이 예제에서는 현재 하나의 사각형만 처리하도록 가정합니다.
-
-        // 선택된 사각형을 드래그할 때 필요한 정보를 저장합니다.
-        if (PtInRect(&dragRect, clientPoint)) {
-            RightButtonActived = 1;
-            startPoint1 = endPoint1 = clientPoint; // 시작 및 끝 포인트를 현재 마우스 위치로 설정합니다.
-        }
-    }
-    break;
-
-    case WM_MOUSEMOVE:
-    {
-        // 마우스 이동 중
-        if (isMouseLButtonPressed)
-        {
-            endPoint.x = LOWORD(lParam);
-            endPoint.y = HIWORD(lParam);
-
-            // WM_PAINT 메시지를 유발하여 네모를 화면에 그립니다.
-            InvalidateRect(hwnd, NULL, TRUE);
-        }
-        if (RightButtonActived)
-        {
-            endPoint1.x = LOWORD(lParam);
-            endPoint1.y = HIWORD(lParam);
-
-            // 현재 마우스 위치와 시작 클릭 위치 사이의 거리를 계산합니다.
-            int deltaX = endPoint1.x - startPoint1.x;
-            int deltaY = endPoint1.y - startPoint1.y;
-
-            // 선택된 사각형을 이동합니다.
-            OffsetRect(&dragRect, deltaX, deltaY);
-
-            startPoint1 = endPoint1; // 현재 마우스 위치를 새로운 시작점으로 설정합니다.
-
-            // 화면 다시 그리기
-            InvalidateRect(hwnd, NULL, TRUE);
-        }
-    }
-    break;
-
-    case WM_LBUTTONUP:
-    {
-        endPoint.x = LOWORD(lParam);
-        endPoint.y = HIWORD(lParam);
-
-        isMouseLButtonPressed = 0;
-    }
-    break;
-
-    case WM_RBUTTONUP:
-    {
-        RightButtonActived = 0;
-    }
-    break;
-    
     case WM_KEYDOWN:
-    {
-        // 방향키 입력 처리
-        int step = 5; // 이동 거리
-        if (wParam == VK_LEFT) {
-            point.x -= step; // 왼쪽으로 이동
+        isKeyPressed = 1;
+        if (wParam == VK_RIGHT)
+        {
+            rect_user.left += 5;
+            rect_user.right += 5;
         }
-        else if (wParam == VK_RIGHT) {
-            point.x += step; // 오른쪽으로 이동
+        else if (wParam == VK_LEFT) // VK_LEFT 키를 누르면 왼쪽으로 이동
+        {
+            rect_user.left -= 5;
+            rect_user.right -= 5;
         }
-        else if (wParam == VK_UP) {
-            point.y -= step; // 위로 이동
+        else if (wParam == VK_DOWN) // VK_DOWN 키를 누르면 아래로 이동
+        {
+            rect_user.top += 5;
+            rect_user.bottom += 5;
         }
-        else if (wParam == VK_DOWN) {
-            point.y += step; // 아래로 이동
+        else if (wParam == VK_UP) // VK_UP 키를 누르면 위로 이동
+        {
+            rect_user.top -= 5;
+            rect_user.bottom -= 5;
         }
-
-        // WM_PAINT 메시지를 유발하여 점을 다시 그립니다.
         InvalidateRect(hwnd, NULL, TRUE);
-    }
-    break;
-
+        break;
+    case WM_KEYUP:
+        isKeyPressed = 0;
+        break;
     case WM_PAINT:
     {
-        PAINTSTRUCT ps;
-        HDC hdc = GetDC(hwnd);
-        // 점 그리기
-        HBRUSH hWhiteBrush = CreateSolidBrush(RGB(255, 255, 255)); // 흰색 브러시 생성
-        FillRect(hdc, &ps.rcPaint, hWhiteBrush); // 클라이언트 영역을 흰색으로 채움
-        DeleteObject(hWhiteBrush); // 브러시 삭제
+        FillRect(hdc, &rect_user, Brush_user);
+        FillRect(hdc, &rect_target, Brush_target);
 
-        Ellipse(hdc, point.x - 5, point.y - 5, point.x + 5, point.y + 5);
-
-        EndPaint(hwnd, &ps);
-
-        
-
-        if (isMouseLButtonPressed)
+        // 사용자 사각형과 타겟 사각형의 충돌을 검사
+        if (RectIntersect(&rect_user, &rect_target))
         {
-            RECT rect;
-            GetClientRect(hwnd, &rect);
-            FillRect(hdc, &rect, (HBRUSH)(COLOR_WINDOW + 1));
-
-            // 드래그 중인 상자 그리기    
-            dragRect.left = min(startPoint.x, endPoint.x);
-            dragRect.top = min(startPoint.y, endPoint.y);
-            dragRect.right = max(startPoint.x, endPoint.x);
-            dragRect.bottom = max(startPoint.y, endPoint.y);
-
-            HBRUSH hBrush = CreateSolidBrush(RGB(255, 0, 255)); // 핑크 브러시 생성
-            FillRect(hdc, &dragRect, hBrush); // 핑크색 상자 그리기
-            DeleteObject(hBrush);
+            TextOut(hdc, 10, 10, text, lstrlen(text));
         }
-
-        // 우클릭으로 드래그하는 사각형 그리기
-        if (RightButtonActived)
-        {
-            RECT rect;
-            GetClientRect(hwnd, &rect);
-            FillRect(hdc, &rect, (HBRUSH)(COLOR_WINDOW + 1));
-
-            HBRUSH hBrush = CreateSolidBrush(RGB(255, 0, 255)); // 핑크 브러시 생성
-            FillRect(hdc, &dragRect, hBrush); // 핑크색 상자 그리기
-            DeleteObject(hBrush);
-        }
-
-        ReleaseDC(hwnd, hdc);
     }
     break;
 
     case WM_DESTROY:
         PostQuitMessage(0);
         break;
+    case WM_CLOSE:
+        DestroyWindow(hwnd);
+        break;
+
 
     default:
         return DefWindowProc(hwnd, uMsg, wParam, lParam);
     }
 
+    DeleteObject(Brush_target);
+    DeleteObject(Brush_user);
+
+
+    ReleaseDC(hwnd, hdc);
+
     return S_OK;
 }
-
 #ifdef UNICODE
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nShowCmd)
 #else
@@ -197,6 +110,7 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
     wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
     wc.hCursor = LoadCursor(NULL, IDC_CROSS);
     wc.style = CS_HREDRAW | CS_VREDRAW;
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
     wc.lpfnWndProc = WindowProc;
 
     // 윈도우 클래스 등록.
@@ -215,10 +129,10 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
     // 윈도우 생성
     HWND hwnd = CreateWindow(
         wc.lpszClassName,
-        TEXT("202107054 이용규 5주차 과제"),
-        WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX | WS_MAXIMIZEBOX, // 크기 조정 및 버튼 비활성화
+        TEXT("202107054 이용규"),
+        WS_OVERLAPPEDWINDOW,
         0, 0,
-        800, 600, // 고정된 창 크기
+        width, height,
         NULL, NULL,
         hInstance,
         NULL
@@ -235,13 +149,15 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
     ShowWindow(hwnd, SW_SHOW); // 창 띄우고
     UpdateWindow(hwnd); // 업데이트해야 보임. 한 쌍으로 쓴다고 보면 됨.
 
+
+
     // 메시지 처리 루프.
     MSG msg;
     ZeroMemory(&msg, sizeof(msg));
 
+    // 메시지 처리.
     while (msg.message != WM_QUIT)
     {
-        // 메시지 처리.
         if (GetMessage(&msg, NULL, 0, 0))
         {
             // 메시지 해석해줘.
@@ -249,8 +165,19 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
             // 메시지를 처리해야할 곳에 전달해줘.
             DispatchMessage(&msg);
         }
+        //if(PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+        //{
+        //   // 메시지 해석해줘.
+        //   TranslateMessage(&msg);
+        //   // 메시지를 처리해야할 곳에 전달해줘.
+        //   DispatchMessage(&msg);
+        //} // PM_REMOVE의 자리는 이 메세지를 쓰고 어떡할거냐.의 의미인데 지운다는 것임.
+        //else{}
     }
 
-    //종료 메시지 보내기 
+    UnregisterClass(wc.lpszClassName, wc.hInstance);
+
+    //종료 메시지 보내기
     return (int)msg.wParam;
+
 }
